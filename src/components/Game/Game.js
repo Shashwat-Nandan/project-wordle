a92c4b8cf35450c8272dcf966b86e2a656337232
Shashwat Nandan <shashwat.nandan@propelytics.in>
2023-09-24 07:21:36 +0530

    exercise-1

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React from 'react'
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from '../../utils'
+import { WORDS } from '../../data'
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({ answer })
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState('')
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault()
+        console.log(guess)
+        setGuess('')
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        minLength={5}
+        maxLength={5}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  )
 }
 
-export default Game;
+export default Game
